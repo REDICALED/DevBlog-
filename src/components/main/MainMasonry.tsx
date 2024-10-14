c6459a3e93df201595213dd154b425bfeed42185
redicaled <redicaled@gmail.com>
2024-10-15 02:03:17 +0900

    masonry

--- src/components/main/MainMasonry.tsx
+++ src/components/main/MainMasonry.tsx
@@ -14,7 +14,7 @@ export default function Notes(props: any) {
                 className="">
                 <Masonry className="px-5 lg:px-10">
                     {props.supaArray.map((value: any, index: number) => (
-                        <Link key={index} href={`/post/${value.uuid}`} className="group relative block h-40 sm:h-64 lg:h-96 overflow-hidden">
+                        <Link key={index} href={`/post/${value.uuid}`} className="group relative block h-40 sm:h-64 lg:h-96 overflow-hidden m-1">
                             { (value.images && value.images.length) > 0 && (
                                 <img
                                     alt={value.title}
@@ -22,8 +22,8 @@ export default function Notes(props: any) {
                                     className="absolute inset-0 h-2/3 w-full object-cover opacity-75 transition-opacity group-hover:opacity-50"
                                 />
                             )}
-                            <span className="absolute inset-0 border-2 border-[var(--text-color)] border-dashed"></span>
-                            <div className="relative flex h-full transform items-end border-2 border-[var(--text-color)] transition-transform">
+                            <span className=" absolute inset-0 border-2 border-[var(--text-color)] border-dashed"></span>
+                            <div className=" relative flex h-full transform items-end border-2 border-[var(--text-color)] transition-transform">
                                 <div className="pb-1 !pt-0 transition-opacity group-hover:absolute group-hover:opacity-0">
                                     <h3 className="p-1 mt-1 text-sm font-bold sm:text-2xl overflow-hidden whitespace-nowrap text-ellipsis">
                                         <p className="p-1 mt-1 break-words">
