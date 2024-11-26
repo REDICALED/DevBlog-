10fc7edb7f510573ff49184571b4e12a7ddbfa75
redicaled <redicaled@gmail.com>
2024-11-26 16:40:11 +0900

    main header 네비게이션 화 -> 스크롤 감지해서 크기 축소

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,6 @@ export default function Home() {
             <div className='row-span-1 place-items-center grid'>
                 <Main_header/>
             </div>
-            <hr className=" h-[2.5px] lg:h-[3.5px] mb-8 lg:mt-8 border-0 " style={{ backgroundColor: 'var(--text-color)' }}/>
             
             <div className=' mb-10'>
             
--- src/components/main/Main_header.tsx
+++ src/components/main/Main_header.tsx
@@ -62,12 +62,11 @@ const handleScroll = () => {
     return (
         <>
             <motion.span
-                className={`flex flex-row justify-center items-center ${openingstate ? 'h-screen' : ` bg-[var(--bg-color)] w-full fixed top-0 z-50 `}`}
+                className={` lg:border-b-[4.0px] border-b-[3.0px] mb-0 border-[var(--text-color)] flex flex-row justify-center items-center ${openingstate ? 'h-screen' : ` bg-[var(--bg-color)] w-full fixed top-0 z-50 `}`}
                 animate={containerControls}  // 컨테이너 애니메이션 제어
             >
               <span className="lg:h-[130px] h-[60px] overflow-hidden lg:mr-5 mr-2 transition-none hover:transition-all hover:duration-200 rounded-md text-[var(--text-color)] hover:text-[var(--bg-color)] hover:bg-[var(--text-color)] inline-flex">
                     <Pirate_logo />
-                    
                 </span>
                 
                 <motion.div
@@ -128,7 +127,7 @@ const handleScroll = () => {
             </motion.span>
             {!openingstate && (
         <div
-            className={`transition-all duration-500 h-[15vh] lg:h-[25vh] `}
+            className={`transition-all duration-500 h-[15vh] lg:h-[25vh] mt-8 `}
         ></div>
     )}
 
