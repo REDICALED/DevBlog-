ecd037a70a0012fe53f9d474007b9be3567f5b64
redicaled <redicaled@gmail.com>
2024-12-11 10:57:08 +0900

    font 로딩 swap, mainheader loading 옵션

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,18 +11,22 @@ const montserrat = Montserrat({
   preload: true,
   subsets: ["latin"], // 또는 preload: false
   weight: ["100","200","300", "400","500","600", "700", "900"], // 가변 폰트가 아닌 경우, 사용할 fontWeight 배열
+  display: "swap", // 폰트 로딩 최적화
 });
-
-const Mainheader = dynamic(() => import('@/components/main/Main_header')
-, {
-  ssr: false, // 클라이언트 사이드에서만 렌더링하도록 설정
-  });
 
 const notoSansKr= Noto_Sans_KR({
   subsets: [], // preload에 사용할 subsets입니다.
   weight: ["100", "400", "700", "900"],
   variable: "--notoSansKr", // CSS 변수 방식으로 스타일을 지정할 경우에 사용합니다.
+  display: "swap", // 폰트 로딩 최적화
+
 });
+
+const Mainheader = dynamic(() => import('@/components/main/Main_header')
+, {
+  ssr: false, // 클라이언트 사이드에서만 렌더링하도록 설정
+  loading: () => <div>Loading...</div>  // 로딩 스피너나 다른 UI를 제공
+  });
 
 export const metadata: Metadata = {
   title: "kob3krnl.0.1-rt24.11",
