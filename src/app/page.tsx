740b3347178634079cfa33a84a1da9f06da98152
redicaled <redicaled@gmail.com>
2025-01-03 07:09:56 +0900

    vercel 에러 디버깅 - 한번에 가져오는 사이즈 줄이기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,40 @@
-//main 페이지 page 컴포넌트
+// main 페이지 page 컴포넌트
 import dynamic from 'next/dynamic'
 import {createClient} from '@/utils/supabase/server'
 
-
 const HeroHeader = dynamic(() => import('@/components/mantine/HeroHeader'), { ssr: false });
-
 const MainPostlist = dynamic(() => import('@/components/main/MainPostlist'), { ssr: false });
+
 export default async function Home() {
     const supabaseClient = createClient();
-    const { data: notes } = await supabaseClient.from('posts').select().order('date', { ascending: false });
-    const SupaArray = await JSON.parse(JSON.stringify(notes));
-    await SupaArray.forEach((value: any, index: number) => {
-        if (value.tags.includes("공지")) {
-            // 해당 값을 배열에서 제거
-            let [removedValue] = SupaArray.splice(index, 1);
-            // 맨 앞에 추가
-            SupaArray.unshift(removedValue);
-    }})
+    const batchSize = 5;  // 한 번에 가져올 데이터 개수
+    let offset = 0;
+    let allData = [];
+
+    // 배치 방식으로 데이터 가져오기
+    while (true) {
+        const { data: notes, error } = await supabaseClient
+            .from('posts')
+            .select()
+            .order('date', { ascending: false })
+            .range(offset, offset + batchSize - 1); // 5개씩 가져옴
+
+        if (error || !notes.length) break;  // 데이터가 더 이상 없으면 종료
+
+        allData = [...allData, ...notes];  // 가져온 데이터를 모두 합침
+        offset += batchSize;  // 다음 배치로 이동
+    }
+
+    // 공지를 맨 앞으로 이동
+    const SupaArray = allData.filter((value: any) => {
+        if (value.tags.includes('공지')) {
+            return true;
+        }
+        return false;
+    });
+    const otherPosts = allData.filter((value: any) => !value.tags.includes('공지'));
+    const finalArray = [...SupaArray, ...otherPosts];  // 공지 먼저, 나머지 뒤에 추가
+
     return (
         <div className=' '>
             <div className='row-span-1 place-items-center grid w-[100vw]'>
@@ -27,7 +45,7 @@ export default async function Home() {
             </div>
             <div className='row-span-1 w-full'>
                 <div className='h-screen'>
-                    <MainPostlist SupaArray={SupaArray}/>
+                    <MainPostlist SupaArray={finalArray}/>
                 </div>
             </div>
         </div>
