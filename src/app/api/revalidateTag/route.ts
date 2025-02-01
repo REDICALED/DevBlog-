83673b71ff04a1a0ddedd718e61ffc9adb47f588
redicaled <redicaled@gmail.com>
2025-02-02 05:55:09 +0900

    revalidate edge remove

--- src/app/api/revalidateTag/route.ts
+++ src/app/api/revalidateTag/route.ts
@@ -1,9 +1,6 @@
 import { NextResponse } from 'next/server';
 import { revalidateTag } from 'next/cache';
 
-export const runtime = 'edge';
-
-
 export async function POST(req: Request): Promise<NextResponse> {
     const { tag } = await req.json();
       await revalidateTag(tag);
