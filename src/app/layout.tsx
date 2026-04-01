109367b66349eb4389b93bfc6847ed919ee3f48d
redicaled <redicaled@gmail.com>
2026-04-02 00:22:06 +0900

    ga4 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,42 +4,38 @@ import RecoilRootWrapper from "@/components/Recoil/RecoilWrapper";
 import CustomCursor from "@/components/CustomCursor";
 import dynamic from "next/dynamic";
 import { Toaster } from "sonner";
+import Script from "next/script";
 
-const Mainheader = dynamic(() => import('@/components/main/Main_header')
-, {
-  ssr: false, // 클라이언트 사이드에서만 렌더링하도록 설정
-  });
+const Mainheader = dynamic(() => import("@/components/main/Main_header"), {
+  ssr: false,
+});
 
-  const CheckingModal = dynamic(() => import('@/components/modal/CheckingModal')
-  , {
-    ssr: false, // 클라이언트 사이드에서만 렌더링하도록 설정
-    });
-  
+const CheckingModal = dynamic(() => import("@/components/modal/CheckingModal"), {
+  ssr: false,
+});
 
 export const metadata: Metadata = {
   title: "Project-自記",
   description: "Bckim's warehouse",
   openGraph: {
-    title: 'Project-自記',
+    title: "Project-自記",
     description: "Bckim's warehouse",
     images: [
       {
-        url: '/thumbnail.png', // Must be an absolute URL
+        url: "/thumbnail.png",
         width: 800,
         height: 600,
       },
-
     ],
-
-    locale: 'utf-8',
-    type: 'website',
+    locale: "utf-8",
+    type: "website",
   },
 };
 
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
-}
+};
 
 export default function RootLayout({
   children,
@@ -48,14 +44,28 @@ export default function RootLayout({
 }>) {
   return (
     <html className="scroll-smooth">
-      <body className=" font-custom main_scrollbar ">
+      <body className="font-custom main_scrollbar">
         <RecoilRootWrapper>
-      <CustomCursor />
-      <Mainheader />
-      <CheckingModal />
-      {children}
-        <Toaster position="top-center" richColors />
-				</RecoilRootWrapper>
+          <CustomCursor />
+          <Mainheader />
+          <CheckingModal />
+          {children}
+          <Toaster position="top-center" richColors />
+        </RecoilRootWrapper>
+
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-SR54J9G9JY"
+          strategy="afterInteractive"
+        />
+        <Script id="ga4" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            window.gtag = gtag;
+            gtag('js', new Date());
+            gtag('config', 'G-SR54J9G9JY');
+          `}
+        </Script>
       </body>
     </html>
   );
